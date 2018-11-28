6c62df2f3a7f0319fd70b83203bdff6292b1218a
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2018-11-28 13:23:02 +0000

    Pequeñas actualizaciones después de volver a la Banana Pi

---
+++ temperatura.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Macro para cada hora activar la bomba y esperar unos segundos para procesar las temperaturas de la placa y actualizarla 
+# en la página web
+curl "http://192.168.1.10/cm?cmnd=backlog%20Power%20on;delay%20100;power%20off"
+sleep 30
+sed -E s/\<b\>..../\<b\>`curl -s http://placa/cm?cmnd=Status%2010|cut -c 69-72`/ /mnt/f/index.html >/mnt/f/index.temp
+mv /mnt/f/index.temp /mnt/f/index.html
+python3 /home/hector/bin/funciones.py Temperatura
