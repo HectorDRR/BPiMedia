0aec3f05ec1c5763afa4dc21a3435ce68a0fcd93
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2019-03-17 19:39:58 +0000

    Un poco de limpieza

--- arrancaftp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo tail -5 /var/log/vsftpd.log|grep 'FAIL DOWNLOAD'
-if [ $? -eq 0 ]
-	then
-		echo $(date) VSFTP con problemas, lo arrancamos de nuevo>>/tmp/ftpcaido.txt
-		sudo service vsftpd stop
-		sudo rm /var/log/vsftpd.log
-		sudo service vsftpd start
-fi
