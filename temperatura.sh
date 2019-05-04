2de7d7ecee2c4a9578262a4e75ea9c4150373eb9
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2019-05-04 15:22:24 +0100

    Controlamos si hay error al obtener la temperatura de la placa para no dejar el index.html tocado

--- temperatura.sh
+++ temperatura.sh
@@ -2,7 +2,11 @@
 # Macro para cada hora procesar las temperaturas de la placa y actualizarla en la página web
 # Ya no es necesario activar la bomba curl "http://bomba/cm?cmnd=backlog%20Power%20on;delay%20100;power%20off"
 #sleep 30
-sed -E s/\<b\>..../\<b\>`curl -s 'http://placa/cm?cmnd=Status%2010'|cut -c 69-72`/ /mnt/f/index.html >/mnt/f/index.temp
-mv /mnt/f/index.temp /mnt/f/index.html
-python3 /home/hector/bin/funciones.py Temperatura
+temp=`curl -s http://placa/cm?cmnd=Status%2010`
+if [ ${#temp} -gt 0 ]
+	then
+	sed -E s/\<b\>..../\<b\>`echo $temp|cut -c 69-72`/ /mnt/f/index.html >/mnt/f/index.temp
+	mv /mnt/f/index.temp /mnt/f/index.html
+	python3 /home/hector/bin/funciones.py Temperatura
+fi
 #echo `date`: Hemos procesado la temperatura de la placa >> /mnt/e/.mini/milog.txt
