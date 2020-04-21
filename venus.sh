c0ad17dfea7896ec249788520a7f6abd57b73958
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2020-04-21 10:47:37 +0100

    Asegurado venus.sh

--- venus.sh
+++ venus.sh
@@ -1,8 +1,8 @@
 # Macro para acceder al Venus GX y obtener datos de la instalación fotovoltaica
 # Nos subscribimos al tópica que nos interesa, en este caso la producción de la FotoVoltaica
-mosquitto_sub -h venus -t N/c8df84d39633/pvinverter/32/Ac/Power -C 1 >/tmp/FV &
-mosquitto_pub -h venus -t R/c8df84d39633/pvinverter/32/Ac/Power -m ''
+mosquitto_sub -h venus -t N/$Victron/pvinverter/32/Ac/Power -C 1 >/tmp/FV &
+mosquitto_pub -h venus -t R/$Victron/pvinverter/32/Ac/Power -m ''
 sleep .5
-mosquitto_sub -h venus -t N/c8df84d39633/battery/512/Soc -C 1 >/tmp/Bat &
-mosquitto_pub -h venus -t R/c8df84d39633/battery/512/Soc -m ''
+mosquitto_sub -h venus -t N/$Victron/battery/512/Soc -C 1 >/tmp/Bat &
+mosquitto_pub -h venus -t R/$Victron/battery/512/Soc -m ''
 sleep .5
