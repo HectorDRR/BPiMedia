9638ee3f0ac2cbd87104d78fa1124c7cd41e8fbf
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2020-04-21 09:35:13 +0100

    Botones: Suprimimos todo lo relacionado con el MQTT ya que ahora se gestiona a través de una variable en la propia placa, Mem1

--- Botones.py
+++ Botones.py
@@ -3,9 +3,6 @@
 """ Inicialmente creada para detectar los Dash Buttons de Amazon y controlar la bomba de recirculación de agua caliente.
 Al amazon elimnar estos boes y forzar su bloqueo de manera remota, solo se usa para detectar la conexión de los equipos de Dácil
 y de cualquier otra MAC que queramos detectar y reflejar en el log.
-Ampliamos su uso para incluir un cliente de MQTT para poder hacer que la placa no se active más cuando ya nos hayamos duchado los tres
-o para volver a activarla
-
 Hay que lanzarla con privilegios para poder poner la red en modo promiscuo y detectar la MAC de los equipos
 Se requiere de una segunda función, Boton_detecta, que es la que se dispara cuando se detecta una llamada ARP
 """
@@ -14,7 +11,6 @@ Se requiere de una segunda función, Boton_detecta, que es la que se dispara cua
 from kamene.all import *
 from funciones import Log
 import datetime, sys, os
-import paho.mqtt.client as mqtt
 
 # Definición de variables para hacer la macro portable entre distintas configuraciones/plataformas
 env = __import__('Entorno_' + sys.platform)
@@ -35,41 +31,10 @@ def Boton_detecta(pkt):
 			Log('Se ha conectado la MAC ' + pkt[ARP].hwsrc, Debug)
 	return
 
-def Leo_Boton(client, userdata, message):
-	""" Esta función es llamada cada vez que llega un mensaje por MQTT del tópico al que nos hemos subscrito.
-		En este caso nos hemos suscrito a dos mensaje, el NOMAS y el POWER ON para controlar cuantos quedan por bañarse
-	"""
-	import json
-	# Lo importamos en formato json
-	mensaje = json.loads(message.payload.decode("utf-8"))
-	if Debug:
-		Log('Debug, mensaje: ' + str(mensaje))
-	# Llamamos a bin/bañados.sh que termina con el proceso de la placa, si está corriendo, crea el fichero '/tmp/TodosBañados' y para la placa
-	if not os.path.exists('/tmp/TodosBañados'):
-		Log('No se va a bañar nadie más, ' + str(os.system('/home/hector/bin/bañados.sh')))
-		client.publish('stat/boton2/NOMAS','1')
-	else:
-		Log('Volvemos a activar la programación de la placa, ', os.remove('/tmp/TodosBañados'))
-		client.publish('stat/boton2/NOMAS','0')
-	return
-
-
 if len(sys.argv) == 2:
 	Debug = True
 else:
 	Debug = False
-# Creo el cliente
-client = mqtt.Client('Mulita')
-# Conecto al broker
-client.connect('192.168.1.8')
-# Asigno la función que va a procesar los mensajes recibidos
-client.on_message = Leo_Boton
-# Lo despertamos de su letargo para que nos responda más rápido, ya que hemos observado que con el sleep activado tarda más tiempo
-#client.publish('cmnd/' + Topico + '/SLEEP', '0')
-# Me subscribo a los tópicos necesarios, Estado y Temperatura
-client.subscribe('cmnd/boton2/NOMAS', 0)
-# Comenzamos el bucle
-client.loop_start()
 
 # Lanzamos el bucle que se queda detectando las llamadas ARP. Este funcionará de manera ininterrumpida
 # Hay que lanzarlo con el arranque del equipo. 
