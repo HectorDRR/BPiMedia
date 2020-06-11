72a70348a38124d60802ee32c2bae45d5ba36bbf
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2020-06-11 18:22:46 +0100

    Incluimos el control del proceso de Carga del coche y mejoramos la búsqueda para hacerla más concreta poniendo solo una instancia, como el caso del smbd o el vsftpd

--- procesos.sh
+++ procesos.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Macro para coprobar que están en funcionamiento los procesos básicos del sistema
-for f in amuled transmiss minidlna smb vsftpd noip2 python3; do
-	pgrep -a $f
+for f in amuled transmiss minidlna smb vsftpd noip2 Botones.py CargaCoche; do
+	pgrep -afn $f
 	if [ $? -eq 1 ] 
 		then
 		echo Existe un problema con el proceso $f
